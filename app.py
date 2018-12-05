948415f6d153db4389b9ee2f1194b8a24d3ad548
anderosnbispos <45535887+anderosnbispos@users.noreply.github.com>
2018-12-05 13:47:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from flask import render_template
+
+color = os.environ.get('APP_COLOR')
+
 @app.route("/")
 def main():
-    return "Welcome!"
+    print(color)
+    return render_template('hello.html', color=color)
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-
-@app.route('/read from database')
-def read():
-    cursor.execute("SELECT * FROM employees")
-    row = cursor.fetchone()
-    result = []
-    while row is not None:
-      result.append(row[0])
-      row = cursor.fetchone()
-
-    return ",".join(result)
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', color=new_color)
 
 if __name__ == "__main__":
     app.run()
